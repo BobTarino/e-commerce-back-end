6a8cbe12a5efa4ff044bdf541b91bf8b93b33f75
Bobby T <bob.tarino@gmail.com>
2021-06-14 17:30:01 -0500

    created API routes for Category Model and GET route for Product model

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,57 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+      attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+      ],
+      include: [
+        {
+            model: Category,
+            attributes: [ 'id', 'category_name']
+        },
+      ]
+  })
+    // remember to always include promise to capture reponse from db call
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      },
+    ]
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
