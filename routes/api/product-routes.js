208988df3e5e834bfdf46403d1122de1a942d9a9
Bobby T <bob.tarino@gmail.com>
2021-06-14 22:24:49 -0500

    force:true

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -81,6 +81,7 @@ router.post('/', (req, res) => {
     }
   */
   Product.create(req.body)
+  
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
